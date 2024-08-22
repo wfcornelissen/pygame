012db8d52d346f4d68e4744f7cc5a7372e40aef9
wfcornelissen <wfcornelissen@gmail.com>
2024-08-22 19:52:27 +0200

    set fps control, added circleshape.py that inherits from sprites etc.

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ from constants import *
 
 def main():
     pygame.init
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    n = 1
-    while n < 5:
+
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(000)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
         
 
